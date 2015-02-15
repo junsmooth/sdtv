69ebc5ec0da4dd477a5aa9e823a6944c7e79df68
daijun <daijun@potevio.com>
2015-02-15 15:56:17 +0800

    add point pojo

---
+++ sdtv/src/main/java/com/potevio/sdtv/service/MapXY.java
@@ -0,0 +1,24 @@
+package com.potevio.sdtv.service;
+
+public class MapXY {
+	// Jingdu, longitude
+	private String x;
+	// weidu ,latitude
+	private String y;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+}
